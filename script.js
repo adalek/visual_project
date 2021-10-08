2c031a300ab0be46ce0d23402a00edee6d258e64
adalek <61150101+adalek@users.noreply.github.com>
2021-10-08 16:31:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 let voice = document.getElementById('voice');
-voice.pause();
+
+
+window.onscroll = function(){
+  scrollingControl(document.documentElement.scrollHeight,2000,voice);
+
+}
+
+let y,d,m;
+function scrollingControl(y,d,m){
+  let scrollY=document.documentElement.scrollTop;
+
+  console.log(scrollY);
+
+  let targetY= y;
+
+  let distance= targetY-scrollY
+  console.log("distance:"+distance);
+   m.play()
+   m.volume=0
+
+  if (distance<d){
+    m.volume= 1-distance/d;
+  }
+
+}
